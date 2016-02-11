1e048202430bafb92cc8c7658f694c9d5b4035d9
Saurabh Singhal <singhal.saurabh.mnnit@gmail.com>
2016-02-11 22:36:22 +0530

    Head Spinning Problem :P

--- TheatreSquare.cpp
+++ TheatreSquare.cpp
@@ -2,9 +2,19 @@
 
 using namespace std;
 
+long long int ceil( long long int a, long long int b ) {
+    return ( ( a + b - 1 ) / b );
+}
+
+long long int minSquares( long long int n, long long int m, long long int a ) {
+    long long int x = ceil( n, a );
+    long long int y = ceil( m, a );
+    return ( x * y );
+}
+
 int main() {
-    int n, m, a;
+    long long int n, m, a;
     cin >> n >> m >> a;
-    cout << ( ( n + a ) / a + ( m + a ) / a ) << endl;
+    cout << minSquares( n, m, a ) << endl;
     return 0;
 }
